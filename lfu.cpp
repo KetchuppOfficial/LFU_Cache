581b9e40ad8d316351c875ff1b80de828980349a
KetchuppOfficial <dolgov04@list.ru>
2022-09-11 19:48:21 +0300

    Driver reads data from standard input stream

--- lfu.cpp
+++ lfu.cpp
@@ -17,10 +17,22 @@ int main ()
 {
     size_t cache_size {};
     std::cin >> cache_size;
-    
+
     Caches::LFU<int> cache {cache_size};
 
-    cache.lookup_update(2, Caches::slow_get_page);
+    int n_hits {};
+    for (;;)
+    {
+        int page {};
+        std::cin >> page;
+
+        if (std::cin.eof())
+            break;
+
+        n_hits += cache.lookup_update(page, Caches::slow_get_page);
+    }
+
+    std::cout << "n_hits = " << n_hits << std::endl;
     
     return 0;
 }
