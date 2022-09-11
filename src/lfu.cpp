c6958a81610318fcc61d68ca471b64c8b8e92e73
KetchuppOfficial <dolgov04@list.ru>
2022-09-12 00:06:19 +0300

    Two kinds of fixes
    1) stdin state is checked by assert() macros;
    2) Input handling corresponds the task criteria.

--- src/lfu.cpp
+++ src/lfu.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include "lfu.hpp"
 
 namespace Caches
@@ -12,22 +13,25 @@ int main ()
 {
     std::size_t cache_size {};
     std::cin >> cache_size;
+    assert (std::cin.good ());
+
+    std::size_t n_keys {};
+    std::cin >> n_keys;
+    assert (std::cin.good ());
 
     Caches::LFU<int> cache {cache_size};
 
     int n_hits {};
-    for (;;)
+    for (auto key_i = 0; key_i < n_keys; key_i++)
     {
         int page {};
         std::cin >> page;
-
-        if (std::cin.eof())
-            break;
+        assert (std::cin.good ());
 
         n_hits += cache.lookup_update(page, Caches::slow_get_page);
     }
 
-    std::cout << "n_hits = " << n_hits << std::endl;
+    std::cout << n_hits << std::endl;
     
     return 0;
 }
