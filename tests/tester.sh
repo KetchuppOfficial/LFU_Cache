d3e6451f67c5366268ddf3582f05e0a3acdb5446
KetchuppOfficial <dolgov04@list.ru>
2022-09-13 11:03:14 +0300

    Project structure changed
    LFU cache, naive LFU cache and ideal cache drivers are separated
    into 3 directories. Each one of them contains an independent
    CMakeLists.txt file. This change in structure led to change in the
    testing script.

--- tests/tester.sh
+++ tests/tester.sh
@@ -5,7 +5,7 @@ then
     echo "Testing script requires only 1 argument (number of tests)"
 else
     n_files=$1
-    
+
     test_dir="tests"
     ans_dir="answers"
     
@@ -17,16 +17,14 @@ else
     python3 test_generator.py ${n_files} ${test_dir}
 
     echo "Building naive LFU..."
-    g++ ../src/lfu_naive.cpp -I../include -o lfu_naive
+    cmake ../lfu_naive -B ../lfu_naive/build
+    cmake --build ../lfu_naive/build
 
     echo "Generating answers..."
     mkdir ${ans_dir}
+    cd ../lfu_naive/build
     for ((i = 0; i < $1; i++))
     do
-        ./lfu_naive ./${test_dir}/test_${i}.txt ./${ans_dir}/answer_${i}.txt
+        ./LFU_Naive ../../tests/${test_dir}/test_${i}.txt ../../tests/${ans_dir}/answer_${i}.txt
     done
-
-    # 4) Remove lfu_naive
-    echo "Removing temporary files..."
-    rm lfu_naive
 fi
