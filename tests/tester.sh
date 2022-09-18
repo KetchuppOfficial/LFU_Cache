b01216fe48f3d74e122c38f17eff6af811615333
KetchuppOfficial <dolgov04@list.ru>
2022-09-18 23:33:35 +0300

    Testing script upgraded
    The script has one more urgument that is capable of cache to test.
    Testing itself is implemented as a separate function.
    .gitignore file changed because of new names for folders with
    answers and results to the generated tests.

--- tests/tester.sh
+++ tests/tester.sh
@@ -4,6 +4,11 @@ green="\033[1;32m"
 red="\033[1;31m"
 default="\033[0m"
 
+common_test_dir="../../../tests/"
+test_dir="tests/"
+ans_dir="answers"
+res_dir="results"
+
 function Build
 {
     cmake -B build
@@ -17,57 +22,74 @@ function Mkdir
     mkdir $1
 }
 
-if [ $# -ne 1 ]
+function Test ()
+{
+    local fast="${1}"
+    local naive="${1}_naive"
+    
+    echo "Building naive version..."
+    cd naive
+    Build
+    echo -en "\n"
+
+    echo "Generating answers..."
+    for ((i = 0; i < ${n_tests}; i++))
+    do
+        ./$naive < ${common_test_dir}${test_dir}test_${i}.txt > ${common_test_dir}${ans_dir}_$1/answer_${i}.txt
+    done
+    echo -en "\n"
+    
+    cd ../..
+
+    echo "Building fast version..."
+    cd fast
+    Build
+    echo -en "\n"
+
+    echo "Testing..."
+    for ((i = 0; i < ${n_tests}; i++))
+    do
+        ./$fast < ${common_test_dir}${test_dir}test_${i}.txt > ${common_test_dir}${res_dir}_$1/result_${i}.txt
+
+        echo -n "Test $((i + 1)): "
+        if diff -Z ${common_test_dir}${ans_dir}_$1/answer_${i}.txt ${common_test_dir}${res_dir}_$1/result_${i}.txt > /dev/null
+        then
+            echo -e "${green}passed${default}"
+        else
+            echo -e "${red}failed${default}"
+        fi
+    done
+
+    cd ../../../tests
+}
+
+if [ $# -ne 2 ]
 then
-    echo "Testing script requires only 1 argument (number of tests)"
+    echo "Testing script requires exactly 2 argument"
 else
-    if [ $1 -le 0 ]
+    if [ $2 -le 0 ]
     then
         echo "The number of tests has to be at least 1"
     else
-        n_tests=$1
+        mode=$1
 
-        test_dir="tests"
-        ans_dir="answers"
-        res_dir="results"
-        
-        Mkdir ${test_dir}
-        Mkdir ${ans_dir}
-        Mkdir ../lfu/${res_dir}
-        
-        echo "Generating tests..."
-        python3 test_generator.py ${n_tests} ${test_dir}
-        echo -en "\n"
+        if [ $mode = "lfu" ] || [ $mode = "belady" ]; then
 
-        echo "Building naive LFU..."
-        cd ../lfu_naive
-        Build
-        echo -en "\n"
+            Mkdir ${test_dir}
 
-        echo "Generating answers..."
-        for ((i = 0; i < ${n_tests}; i++))
-        do
-            ./LFU_Naive < ../../tests/${test_dir}/test_${i}.txt > ../../tests/${ans_dir}/answer_${i}.txt
-        done
-        echo -en "\n"
+            echo "Generating tests..."
+            n_tests=$2
+            python3 test_generator.py ${n_tests} ${test_dir}
+            echo -en "\n"
+            
+            Mkdir "${ans_dir}_${mode}"
+            Mkdir "${res_dir}_${mode}"
+            
+            cd "../$mode"
+            Test $mode
 
-        echo "Building O(1) LFU..."
-        cd ../../lfu
-        Build
-        echo -en "\n"
-
-        echo "Testing..."
-        for ((i = 0; i < ${n_tests}; i++))
-        do
-            ./LFU_Cache < ../../tests/${test_dir}/test_${i}.txt > ../${res_dir}/result_${i}.txt
-
-            echo -n "Test $((i + 1)): "
-            if diff -Z ../../tests/${ans_dir}/answer_${i}.txt ../${res_dir}/result_${i}.txt > /dev/null
-            then
-                echo -e "${green}passed${default}"
-            else
-                echo -e "${red}failed${default}"
-            fi
-        done
+        else
+            "There is no testing mode with name $2"
+        fi
     fi
 fi
