6106fef06e6e4b37daa2c7f6fb01fc8692da093d
KetchuppOfficial <dolgov04@list.ru>
2022-09-18 22:27:17 +0300

    Naive and slow implementation of Belady's algorithm
    This version of the ideal cache is needed for test generation.

---
+++ ideal_cache_naive/src/ideal_cache_naive.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cassert>
+#include "ideal_cache_naive.hpp"
+
+namespace Caches
+{
+
+int slow_get_page (int key) { return key; }
+
+}
+
+int main ()
+{
+    std::size_t cache_size {};
+    std::cin >> cache_size;
+    assert (std::cin.good ());
+
+    std::size_t n_keys {};
+    std::cin >> n_keys;
+    assert (std::cin.good ());
+
+    std::vector<int> keys_arr;
+    for (auto key_i = 0; key_i < n_keys; key_i++)
+    {
+        int key{};
+        std::cin >> key;
+        assert (std::cin.good ());
+
+        keys_arr.push_back (key);
+    }
+
+    Caches::Ideal_Cache_Naive<int> cache {cache_size, keys_arr.begin (), keys_arr.end ()};
+
+    int n_hits {};
+    for (auto key_i = 0; key_i < n_keys; key_i++)
+        n_hits += cache.lookup_update(Caches::slow_get_page);
+    
+    std::cout << n_hits << std::endl;
+
+    return 0;
+}
---
+++ include/ideal_cache_naive.hpp
@@ -0,0 +1,137 @@
+#ifndef IDEAL_CACHE_NAIVE_HPP
+#define IDEAL_CACHE_NAIVE_HPP
+
+#include <vector>
+#include <iterator>
+
+#ifdef DUMP
+#include <algorithm>
+#endif
+
+namespace Caches
+{
+
+enum Next
+{
+    no_next = -1
+};
+
+using Vector_Iter = typename std::vector<int>::iterator;
+
+template <typename Page_T, typename Key_T = int, typename Keys_Iter = Vector_Iter>
+class Ideal_Cache_Naive
+{
+    std::size_t capacity_;
+
+    struct Node
+    {
+        Page_T page_;
+        Key_T key_;
+    };
+
+    using Cache_Iter = typename std::vector<Node>::iterator;
+    using Const_Cache_Iter = typename std::vector<Node>::const_iterator;
+
+    std::vector<Node> cache_;
+    Keys_Iter input_iter_;
+    Keys_Iter end_;
+
+public:
+
+    Ideal_Cache_Naive (std::size_t capacity, Keys_Iter begin, Keys_Iter end)
+                      : capacity_{capacity}, input_iter_{begin}, end_{end} {}
+
+    std::size_t size () const { return cache_.size (); }
+
+    bool is_full () const { return (size () == capacity_); }
+
+    template <typename F> bool lookup_update (const F &slow_get_page)
+    {
+        Key_T key {*input_iter_++};
+
+        auto hit = find_by_key (key);
+
+        if (hit == cache_.end ())
+        {           
+            const auto input_next_occurrence = find_next_occurence (key);
+
+            if (input_next_occurrence != static_cast<int>(no_next))
+            {
+                if (is_full ())
+                {
+                    auto latest = find_key_with_latest_occurrence ();
+
+                    if (latest.next_occurrence != static_cast<int>(no_next) &&
+                        latest.next_occurrence < input_next_occurrence)
+                    {
+                        return false;
+                    }
+                    else
+                        cache_.erase (latest.iter);
+                }
+
+                cache_.push_back ({slow_get_page (key), key});
+            }
+            
+            return false;
+        }
+        else
+            return true;
+    }
+
+private:
+
+    auto find_by_key (const Key_T key) const
+    {
+        for (auto iter = cache_.begin (); iter != cache_.end (); iter++)
+        {
+            if (iter->key_ == key)
+                return iter;
+        }
+
+        return cache_.end ();
+    }
+
+    int find_next_occurence (const Key_T key) const
+    {      
+        for (auto iter = input_iter_; iter != end_; iter++)
+        {
+            if (*iter == key)
+                return 1 + static_cast<int>(std::distance (input_iter_, iter));
+        }
+
+        return static_cast<int>(no_next);
+    }
+
+    struct Latest
+    {
+        Const_Cache_Iter iter;
+        int next_occurrence;
+    };
+
+    Latest find_key_with_latest_occurrence () const
+    {
+        auto latest_occurrence = 0;
+        auto node_iter         = cache_.begin ();
+        auto latest_iter       = node_iter;
+        
+        for (; node_iter != cache_.end (); node_iter++)
+        {
+            auto next_occurence = find_next_occurence (node_iter->key_);
+
+            if (next_occurence == static_cast<int>(no_next))
+                return {node_iter, next_occurence};
+            else if (next_occurence > latest_occurrence)
+            {
+                latest_iter       = node_iter;
+                latest_occurrence = next_occurence;
+            }
+        }
+
+        return {latest_iter, latest_occurrence};
+    }
+};
+
+}
+
+#endif // IDEAL_CACHE_NAIVE_HPP
